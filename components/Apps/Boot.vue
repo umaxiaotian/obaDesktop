2b27804df4bdd1019ee148d00cdd2136ec3e33bf
小畑佑馬 <yuumaobata1008@outlook.com>
2023-02-13 00:13:10 +0900

    アイコン変更

--- components/Apps/Boot.vue
+++ components/Apps/Boot.vue
@@ -5,7 +5,7 @@
         <div class="col-sm-auto bg-light sticky-top  ">
             <div class="d-flex flex-sm-column flex-row flex-nowrap bg-light align-items-center sticky-top">
                 <a href="/" class="d-block p-3 link-dark text-decoration-none" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Icon-only">
-                    <i class="bi-bootstrap fs-1"></i>
+                    <i class="bi bi-pc-display fs-1"></i>
                 </a>
                 <ul class="nav nav-pills nav-flush flex-sm-column flex-row flex-nowrap mb-auto mx-auto text-center justify-content-between w-100 px-3 align-items-center">
                     <li class="nav-item">
