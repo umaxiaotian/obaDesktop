d9e75c13fba3f69ed4180bff7a2c3e9afc8bd78c
小畑佑馬 <yuumaobata1008@outlook.com>
2023-02-14 04:29:49 +0900

    高さが収まるように指定

--- components/Apps/Boot.vue
+++ components/Apps/Boot.vue
@@ -1,10 +1,10 @@
 <template  >
-  <div class="cc">
-  <div class="container-fluid" >
-    <div class="row vh-100" >
-        <div class="col-sm-auto bg-light sticky-top  ">
-            <div class="d-flex flex-sm-column flex-row flex-nowrap bg-light align-items-center sticky-top">
-                <a href="/" class="d-block p-3 link-dark text-decoration-none" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Icon-only">
+ 
+  <div class="container-fluid cc " >
+    <div class="row " style="height: 100%;" >
+        <div class="col-sm-auto bg-light  ">
+            <div class="d-flex flex-sm-column">
+                <a href="/" class="p-3 link-dark " title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Icon-only">
                     <i class="bi bi-pc-display fs-1"></i>
                 </a>
                 <ul class="nav nav-pills nav-flush flex-sm-column flex-row flex-nowrap mb-auto mx-auto text-center justify-content-between w-100 px-3 align-items-center">
@@ -59,7 +59,7 @@
           </div>
     </div>
 </div>
-</div>
+
   </template>
   <style scoped>
 .cc {
