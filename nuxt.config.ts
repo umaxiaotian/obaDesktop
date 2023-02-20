1134def192e7cfcf105eb198fc3c1e0c32930993
小畑佑馬 <yuumaobata1008@outlook.com>
2023-02-21 07:04:24 +0900

    タイトルを設定しました

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,12 @@ export default defineNuxtConfig({
         "bootstrap/dist/css/bootstrap.min.css",
         "bootstrap-icons/font/bootstrap-icons.css",
     ],
+    app: {
+        head: {
+          title: 'obaDesktop',
+          meta: [{ name: 'description', content: 'WEB上で使用できるウィンドウコンポーネントです' }],
+        },
+      },
     runtimeConfig: {
         public: {
             RELEASE_DATE: process.env.RELEASE_DATE
